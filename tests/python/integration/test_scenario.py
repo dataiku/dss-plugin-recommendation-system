98cff1409957961eca853197a803ee18754d9ceb
HenriChabert <henri@famille-chabert.com>
2021-05-18 11:51:40 +0200

    Capitalize scenario id

--- tests/python/integration/test_scenario.py
+++ tests/python/integration/test_scenario.py
@@ -3,9 +3,11 @@ from dku_plugin_test_utils import dss_scenario
 
 TEST_PROJECT_KEY = "RECOTEST"
 
+
 def add_integration_test(user_dss_clients, scenario_id):
     dss_scenario.run(user_dss_clients, project_key=TEST_PROJECT_KEY, scenario_id=scenario_id)
 
 
 def test_all_builds(user_dss_clients):
-    add_integration_test(user_dss_clients, "all_build")
+    add_integration_test(user_dss_clients, "ALL_BUILD")
+
